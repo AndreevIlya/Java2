e5ebb5d7b47acc781c89ce440128804a95a7de2a
AndreevIlya <katauri@yandex.ru>
2019-03-03 15:41:41 +0300

    Try-with-resources added.

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,16 @@
+import java.io.IOException;
+
 public class Main {
 
     public static void main(String[] args) {
+        /*
+        * Add more rows or items to any row to get a MyArraySizeException.
+        * Substitute any number on anything NaN or just delete a number to get
+        * a MyArrayDataException with respective message.
+        * Leave it as it is to pass without exceptions.
+        */
         String[][] array = {{"1","2","3","4"},
-                {"1","","3","4"},
+                {"1","2","3","4"},
                 {"1","2","3","4"},
                 {"1","2","3","4"}};
         try{
@@ -16,5 +24,14 @@ public class Main {
             System.out.println(exc.getMessage());
         }
 
+        System.out.println();
+
+        try(MyClass object = new MyClass()){
+            object.read(5); //set argument to 1 to avoid an IOException
+        }catch(IOException exc){
+            System.out.println("Unable to read.");
+        }catch (Exception exc){
+            System.out.println("Unable to close.");
+        }
     }
 }
