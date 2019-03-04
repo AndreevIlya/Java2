787520872a8485c24e90f240fc09161d906e01f8
AndreevIlya <katauri@yandex.ru>
2019-03-04 18:29:31 +0300

    Show method split into two: show and showLine. An exception through an exception way adopted.

--- src/MyArray.java
+++ src/MyArray.java
@@ -22,36 +22,37 @@ class MyArray {
     void show(){
         System.out.print("{");
         for(int i = 0; i < arraySizeLimit; i++){
-            System.out.print("{");
-            for(int j = 0; j < arraySizeLimit; j++){
-                if(j != arraySizeLimit - 1) {
-                    System.out.print(arrayString[i][j] + ", ");
-                }else{
-                    System.out.print(arrayString[i][j]);
-                }
-            }
-            if(i != arraySizeLimit - 1){
-                System.out.println("},");
-            }else{
-                System.out.print("}");
-            }
+            showLine(i);
         }
         System.out.println("}");
+    }
+
+    void showLine(int i){
+        System.out.print("{");
+        for(int j = 0; j < arraySizeLimit; j++){
+            if(j != arraySizeLimit - 1) {
+                System.out.print(arrayString[i][j] + ", ");
+            }else{
+                System.out.print(arrayString[i][j]);
+            }
+        }
+        if(i != arraySizeLimit - 1){
+            System.out.println("},");
+        }else{
+            System.out.print("}");
+        }
     }
 
     void arrayToInt() throws MyArrayDataException{
         for(int i = 0; i < arraySizeLimit; i++){
             for(int j = 0; j < arraySizeLimit; j++){
                 if(!arrayString[i][j].equals("")) {
-                    for (int k = 0; k < arrayString[i][j].length(); k++) {
-                        char c = arrayString[i][j].charAt(k);
-                        if (!(c == '1' || c == '2' || c == '3' || c == '4' || c == '5' ||
-                                c == '6' || c == '7' || c == '8' || c == '9' || c == '0')) {
-                            throw new MyArrayDataException("In the cell at " + (i + 1) +
-                                    " " + (j + 1) + " there\'s no number present");
-                        }
+                    try{
+                        arrayInt[i][j] = parseInt(arrayString[i][j]);
+                    }catch(NumberFormatException exc){
+                        throw new MyArrayDataException("In the cell at " + (i + 1) +
+                                " " + (j + 1) + " there\'s no number present");
                     }
-                    arrayInt[i][j] = parseInt(arrayString[i][j]);
                 }else{
                     throw new MyArrayDataException("The cell at " + (i + 1) +
                             " " + (j + 1) + " is empty");
