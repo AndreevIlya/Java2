b070108353891bde8c817c88f006941565177141
AndreevIlya <katauri@yandex.ru>
2019-03-13 23:14:00 +0300

    Strange behavior in Interrupted corrected.

--- src/Interrupted.java
+++ src/Interrupted.java
@@ -1,23 +1,20 @@
-/*
-* Sometimes during sleep exception is not thrown.
-* */
-
 import java.util.Random;
 
 public class Interrupted implements Runnable {
     private int counter = 0;
-    private Random rand = new Random();
+    private static Random rand = new Random();
+    private static int randomNumber = rand.nextInt(100);
 
     @Override
     public void run(){
-        while(counter < 1100){
+        while(counter < 110){
             counter++;
-            if(counter % 100 == 0) System.out.println(counter);
-            if(counter == rand.nextInt(1000)){
+            if(counter % 10 == 0) System.out.println(counter);
+            if(counter == randomNumber){
                 Thread.currentThread().interrupt();
             }
             try {
-                Thread.sleep(20);
+                Thread.sleep(200);
             }catch (InterruptedException exc){
                 System.out.println("Thread is interrupted at " + counter);
                 return;
